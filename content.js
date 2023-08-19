9ea715b8c67edf5df30a7e2fee7687beccdf7163
Devin Carter <devinacarter97@gmail.com>
2023-08-19 18:52:26 +0100

    Updated banner and added password match warning

--- content.js
+++ content.js
@@ -1,24 +1,28 @@
 console.log("hi");
 let password = document.getElementById("set-password");
 let confirmPassword = document.getElementById("confirm-password");
-
-console.log({ password });
+let invalidPassword = document.getElementById("invalid-passwords");
 
 let submit = document.getElementById("submit");
 
-password.addEventListener("mouseout", function () {
-  let value = document.getElementById("set-password").value;
-  console.log({ value });
-});
-
 submit.addEventListener("click", validatePassword);
+
+//submit.addEventListener("submit", function (event) {
+//  let value = document.getElementById("set-password").value;
+//let connfirmVal = document.getElementById("confirm-password").value;
+//if (value != connfirmVal) {
+// event.preventDefault();
+//} else {
+//submit.attachEvent("onsubmit", function (evt) {
+//evt.preventDefault();
+//});
+//}
+//});
 
 function validatePassword() {
   let value = document.getElementById("set-password").value;
   let connfirmVal = document.getElementById("confirm-password").value;
-
   if (value != connfirmVal) {
-    submit.textContent = "Passwords must match";
-    console.log("Password must match");
+    invalidPassword.textContent = "Passwords must match";
   }
 }
